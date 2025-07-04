8c935b99a469597916636e74056a075e272479ab
xvideosman <xvideosman228@gmail.com>
2025-07-04 23:18:15 +0300

    Сделаны функции-генераторы пресетов цветных клавиатур

---
+++ handlers/fanConfigMenuHandler.py
@@ -0,0 +1,53 @@
+from aiogram.filters import StateFilter, or_f
+from aiogram.fsm.context import FSMContext
+from aiogram.filters.command import Command
+from aiogram import Router, F
+from aiogram.types import Message
+from keyboard import
+from stateMachine import StateMachine
+from config.loggingConfig import exception, logger
+import json
+
+with open('../config/buttons.json') as file:
+    names = json.load(file)
+
+with open('../config/texts.json') as file:
+    texts = json.load(file)
+
+startStates = (StateMachine.FAN_CONFIG, StateMachine.ABOUT)
+
+startMenuRouter = Router()
+
+@exception
+@startMenuRouter.message(Command('start'))
+async def start(message: Message, state: FSMContext):
+    await state.set_state(StateMachine.START)
+    await message.answer(texts["start"], reply_markup=startKeyboard)
+
+@exception
+@startMenuRouter.message(StateFilter(StateMachine.START), F.text == names["fanConfig"])
+async def fanConfig(message: Message, state: FSMContext):
+    logger.info("Кнопка Fan Config нажата")
+    await state.set_state(StateMachine.FAN_CONFIG)
+    await message.answer(texts["fanConfig"], reply_markup=fanConfigKeyboard)
+
+@exception
+@startMenuRouter.message(StateFilter(StateMachine.START), F.text == names["about"])
+async def about(message: Message, state: FSMContext):
+    logger.info("Кнопка About нажата")
+    await state.set_state(StateMachine.ABOUT)
+    await message.answer(texts["about"], reply_markup=backKeyboard)
+
+@exception
+@startMenuRouter.message(or_f(*startStates), F.text == names["back"])
+async def back(message: Message, state: FSMContext):
+    await state.set_state(StateMachine.START)
+    await message.answer(texts['start'], reply_markup=startKeyboard)
+
+@exception
+@startMenuRouter.message(StateFilter(StateMachine.START), F.text)
+async def default(message: Message, state: FSMContext):
+    logger.info(f"Пользователь {message.from_user.id} чёто непонятное сказал")
+    await state.set_state(StateMachine.START)
+    await message.answer(texts['iDidNotFuckingUnderstandYouStupidMoron'], reply_markup=startKeyboard)
+
--- keyboard.py
+++ keyboard.py
@@ -1,5 +1,6 @@
 from aiogram.types import ReplyKeyboardMarkup, KeyboardButton as Key, InlineKeyboardButton as Ikey, \
     InlineKeyboardMarkup
+from aiogram.utils.keyboard import ReplyKeyboardBuilder
 from functools import partial
 import json
 
@@ -17,6 +18,27 @@ startKeyboard = ReplyKeyboard(keyboard=[
 fanConfigKeyboard = ReplyKeyboard(keyboard=[
         [Key(text=names['fans']['preinstalled'])], [Key(text=names['fans']['colors'])], [Back()]])
 
+def generatePreinstalledFanConfigKeyboard():
+    builder = ReplyKeyboardBuilder()
+    for text in names['preinstalled'].values():
+        builder.button(text=f"{text}")
+    builder.add(Back())
+    builder.adjust(3,3,3, 1)
+
+    return builder.as_markup()
+
+preinstalledFanConfigKeyboard = generatePreinstalledFanConfigKeyboard()
+
+def generateColorsFanConfigKeyboard():
+    builder = ReplyKeyboardBuilder()
+    for text in names['colors']['basicColors'].values():
+        builder.button(text=f"{text}")
+    builder.add(Back())
+    builder.adjust(3, 1)
+
+    return builder.as_markup()
+
+colorsFanConfigKeyboard = generateColorsFanConfigKeyboard()
 
 backKeyboard = ReplyKeyboard(keyboard=[
         [Back()]])
