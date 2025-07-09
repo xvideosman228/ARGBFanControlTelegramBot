ed0127e30b3e75cd137f7449dbaf0e316bfe8dbf
xvideosman <xvideosman228@gmail.com>
2025-07-09 12:04:11 +0300

    Fade in & out теперь имеет длительность

--- stateMachine.py
+++ stateMachine.py
@@ -5,8 +5,10 @@ class StateMachine(StatesGroup):
     START = State()
     CUSTOM_PRESETS = State()
 
+    COLOR_FADE_IN_OUT = State()
+    TIME_FADE_IN_OUT = State()
     FADE_IN_OUT = State()
 
-    COLOR_1 = State()
+    COLOR_1_COLOR_WIPE = State()
     COLOR_WIPE_1 = State()
     COLOR_WIPE_2 = State()
